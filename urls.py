21b4862b1001a3265ac5f8b09010a913279653fe
onurmatik <omat@gezgin.com>
2016-02-01 22:56:30 +0200

    admin site tile updated

--- urls.py
+++ urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
 
+admin.site.site_header = _('Social Suite')
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
